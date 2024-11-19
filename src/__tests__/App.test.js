7222a754dc723152bfaff916e6cbd6f0c6ff6c54
Adi K <102898613+adikul1023@users.noreply.github.com>
2024-11-19 11:12:36 +0530

    singleton App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Adi Kulkarni");
+    expect(document.title).toBe('Adi Kulkarni');
   });
 
   it('can navigate to /about', async () => {
